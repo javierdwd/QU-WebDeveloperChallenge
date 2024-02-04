cb2594af8717787b45f973aed2a46ee646f62ce8
Javier Van Breedan <javierdwd@gmail.com>
2024-02-04 16:11:34 -0300

    [Swapi] Create the package and some utils like typing info ad texts.

---
+++ packages/Swapi/src/texts.ts
@@ -0,0 +1,7 @@
+// In a more advanced example I would use i18n/json files to store this kind of texts.
+export const TextsMap = {
+  "diameter": "Diameter",
+  'climate': "Climate",
+  'terrain': "Terrain",
+  'population': "Population",
+}
